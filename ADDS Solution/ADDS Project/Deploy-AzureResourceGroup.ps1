49d32ff231076d2b4abb436055de4589914a1af0
chhatley <48930235+chhatley@users.noreply.github.com>
2019-04-01 19:17:48 -0400

    fd

--- ADDS Solution/ADDS Project/Deploy-AzureResourceGroup.ps1
+++ ADDS Solution/ADDS Project/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'AzureADDSSolution',
+    [string] $ResourceGroupName = 'ADDSSolution',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
