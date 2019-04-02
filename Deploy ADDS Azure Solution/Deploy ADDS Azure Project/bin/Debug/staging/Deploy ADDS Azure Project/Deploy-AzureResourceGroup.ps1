29d02b18ba53acdbbda61ed08181aa415ee42646
chhatley <48930235+chhatley@users.noreply.github.com>
2019-04-02 12:26:36 -0400

    Huh

--- Deploy ADDS Azure Solution/Deploy ADDS Azure Project/bin/Debug/staging/Deploy ADDS Azure Project/Deploy-AzureResourceGroup.ps1
+++ Deploy ADDS Azure Solution/Deploy ADDS Azure Project/bin/Debug/staging/Deploy ADDS Azure Project/Deploy-AzureResourceGroup.ps1
@@ -6,8 +6,8 @@ Param(
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
-    [string] $TemplateFile = 'azuredeploy.json',
-    [string] $TemplateParametersFile = 'azuredeploy.parameters.json',
+    [string] $TemplateFile = 'deployaddsmaster.json',
+    [string] $TemplateParametersFile = 'deployaddsmaster.parameters.json',
     [string] $ArtifactStagingDirectory = '.',
     [string] $DSCSourceFolder = 'DSC',
     [switch] $ValidateOnly
